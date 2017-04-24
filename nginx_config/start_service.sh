205b419586eca8d3a9ed77e0860e51eaadcefbd4
Sergio Slobodrian <sslobodr@ciena.com>
2017-04-24 16:33:39 -0400

    Added the voltha swagger interface to the proxy. Voltha's swagger interface is now avaialble at http://localhost/swagger. Made a small change to the way nginx is started because requests to reload the config were comming in before it had finished loading its config causing consul-template to choke. Added a gitignore file in the upstreams directory to ensure that it gets created in the repo. It needs to be there since the directory is mounted by the container during the development phase.
    Change-Id: I39a1683970ba114f5952913a5beb7cfe9cfe1cc3

--- nginx_config/start_service.sh
+++ nginx_config/start_service.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cd /
-#nginx -c /nginx_config/nginx.conf&
-exec consul-template -reload-signal='SIGHUP' -consul $CONSUL_ADDR -template="nginx_config/nginx-upstreams.ctmpl:nginx_config/upstreams/voltha-upstreams.conf:nginx -s reload"  -exec='/usr/sbin/nginx -c /nginx_config/nginx.conf -g "daemon off;"'
+nginx -c /nginx_config/nginx.conf&
+sleep 2 # Let nginx get settled bfore starting consul-template to avoid attemtps to reload resulting in errors.
+exec consul-template -reload-signal='SIGHUP' -consul $CONSUL_ADDR -template="nginx_config/nginx-upstreams.ctmpl:nginx_config/upstreams/voltha-upstreams.conf:nginx -s reload"
