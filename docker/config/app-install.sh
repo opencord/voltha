9f2ec3cb7a20d0f0763c42c39d4e634431ff1d2e
David K. Bainbridge <dbainbri@ciena.com>
2017-08-10 09:13:18 -0700

    VOL-373 support 3 tuple and multiple version numbers for ONOS apps
    Change-Id: I2e5407fc310eea0111822fd210b83c930053ccb9

--- docker/config/app-install.sh
+++ docker/config/app-install.sh
@@ -1,15 +1,23 @@
-touch $APPS/org.onosproject.openflow-base/active
+#!/bin/bash 
 
-find $ONOS -name "*.oar" -path "*/target/*" | while read line; do 
-    mkdir -p $AUX && cd $AUX 
-    cp $line $AUX
-    jar -xf $AUX/*.oar
-    name=$(grep "name=" $AUX/app.xml | sed 's/<app name="//g;s/".*//g')
-    mkdir -p $APPS/$name
-    cp $AUX/app.xml $APPS/$name/app.xml
-    touch $APPS/$name/active
-    [ -f $AUX/app.png ] && cp $AUX/app.png $APPS/$name/app.png
-    cp $AUX/*.oar $APPS/$name/$name.oar
-    cp -rf $AUX/m2/* $KARAF_M2
-    rm -fr $AUX
+for app in $APPS; do
+    echo "Installing application '$app'"
+    oar=$(find $BUILD_ROOT -path "*/target/*" -name "$app*".oar)
+    if [ "$oar x" == " x" ]; then
+        echo "Required application, $app, not found."
+	continue
+    fi
+    rm -rf $APP_INSTALL_ROOT
+    mkdir -p $APP_INSTALL_ROOT
+    cd $APP_INSTALL_ROOT
+    cp $oar $APP_INSTALL_ROOT
+    jar -xf $APP_INSTALL_ROOT/$(basename $oar)
+    name=$(grep "name=" $APP_INSTALL_ROOT/app.xml | sed 's/<app name="//g;s/".*//g')
+    mkdir -p $APPS_ROOT/$name
+    cp $APP_INSTALL_ROOT/app.xml $APPS_ROOT/$name/app.xml
+    touch $APPS_ROOT/$name/active
+    [ -f $APP_INSTALL_ROOT/app.png ] && cp $APP_INSTALL_ROOT/app.png $APPS_ROOT/$name/app.png
+    cp $APP_INSTALL_ROOT/$(basename $oar) $APPS_ROOT/$name/$name.oar
+    cp -rf $APP_INSTALL_ROOT/m2/* $KARAF_M2
+    rm -rf $APP_INSTALL_ROOT
 done
