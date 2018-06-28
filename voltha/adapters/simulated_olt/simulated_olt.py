1f56344588157eea2c6f9d21a247b38a8c9b4584
Jonathan Hart <jono@opennetworking.org>
2018-06-28 08:47:42 -0700

    VOL-809: Use device MAC address if one exists, otherwise generate one.
    Change-Id: Ia64f08ae1b7444af9a38d00960a71d5f76f0c46b

--- voltha/adapters/simulated_olt/simulated_olt.py
+++ voltha/adapters/simulated_olt/simulated_olt.py
@@ -401,7 +401,8 @@ class SimulatedOltAdapter(object):
         # that will correspond to the NNI port
         yield asleep(0.05)
         logical_device_id = uuid4().hex[:12]
-        ld_mac = ':'.join([a+b for a,b in zip(logical_device_id[::2], logical_device_id[1::2])])
+        dpid = device.mac_address if device.mac_address else \
+                ':'.join([a+b for a,b in zip(logical_device_id[::2], logical_device_id[1::2])])
         ld = LogicalDevice(
             # not setting id and datapth_id will let the adapter agent pick id
             desc=ofp_desc(
@@ -422,7 +423,7 @@ class SimulatedOltAdapter(object):
             ),
             root_device_id=device.id
         )
-        ld_initialized = self.adapter_agent.create_logical_device(ld, ld_mac)
+        ld_initialized = self.adapter_agent.create_logical_device(ld, dpid=dpid)
 
         cap = OFPPF_1GB_FD | OFPPF_FIBER
         self.adapter_agent.add_logical_port(ld_initialized.id, LogicalPort(
