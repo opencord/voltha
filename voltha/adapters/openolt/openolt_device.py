905b840780917b14931ce9de8e200e8493fa39b4
Shad Ansari <shad@opennetworking.org>
2018-07-03 00:04:50 +0000

    VOL-1072 OpenOLT - Minimal handling of alarm indication
    OpenOlt driver is sending alarm indications to the adapter
    which are currently not handled. Adding minimal handling
    of the alarm to avoid logging of unknown indication.
    
    Change-Id: Ieb7cb5e5097745a50aea8d415f2f4112feb72478

--- voltha/adapters/openolt/openolt_device.py
+++ voltha/adapters/openolt/openolt_device.py
@@ -191,6 +191,8 @@ class OpenoltDevice(object):
                 elif ind.HasField('flow_stats'):
                     reactor.callFromThread(self.flow_statistics_indication,
                                            ind.flow_stats)
+                elif ind.HasField('alarm_ind'):
+                    self.log.info('alarm indication not handled')
                 else:
                     self.log.warn('unknown indication type')
 
