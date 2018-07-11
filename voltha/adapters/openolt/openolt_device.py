3005f6dd00576bd7b9a6dd65f14277e39072a6f2
Shad Ansari <shad@opennetworking.org>
2018-07-11 17:06:34 +0000

    Revert "VOL-1030 Ignore bulk flow update if OLT is operational down."
    This reverts commit c400bed3fe9790f00629d0be5c157a66f1723b4f.
    
    This patch is likely working around the symptoms of the real issue.
    Reverting so that we can fix the root cause of the voltha hang when
    the openolt driver is killed (or OLT is restarted).
    
    Change-Id: I276aa841587af13d6f4c8016dd02e8896c7ea647

--- voltha/adapters/openolt/openolt_device.py
+++ voltha/adapters/openolt/openolt_device.py
@@ -856,10 +856,6 @@ class OpenoltDevice(object):
                 hex(ord(vendor_specific[3]) & 0x0f)[2:]])
 
     def update_flow_table(self, flows):
-        if self.is_down():
-            self.log.info('OLT is down, ignore update flow table')
-            return
-
         device = self.adapter_agent.get_device(self.device_id)
         self.log.debug('update flow table', number_of_flows=len(flows))
         in_port = None
